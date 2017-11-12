d157dab01a233798a001b85ff5395e05d3898ea2
mgad <mohammedgad09@gmail.com>
2017-11-12 12:23:50 +0300

    Create models

--- tms/project/models.py
+++ tms/project/models.py
@@ -3,4 +3,97 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-# Create your models here.
+class Project(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(db_column='Name', max_length=250)  # Field name made lowercase.
+    start = models.DateField(db_column='Start')  # Field name made lowercase.
+    end = models.DateField()
+    teamname = models.CharField(db_column='TeamName', max_length=100)  # Field name made lowercase.
+    desc = models.CharField(db_column='Desc', max_length=250)  # Field name made lowercase.
+    creationby = models.CharField(db_column='CreationBy', max_length=20)  # Field name made lowercase.
+    creationdate = models.DateTimeField(db_column='CreationDate')  # Field name made lowercase.
+    departementid = models.IntegerField(db_column='DepartementId')  # Field name made lowercase.
+    statusid = models.IntegerField(db_column='StatusId')  # Field name made lowercase.
+    openby = models.CharField(db_column='OpenBy', max_length=20)  # Field name made lowercase.
+    opendate = models.DateTimeField(db_column='OpenDate', blank=True, null=True)  # Field name made lowercase.
+    closedby = models.CharField(db_column='ClosedBy', max_length=20)  # Field name made lowercase.
+    closeddate = models.DateTimeField(db_column='ClosedDate', blank=True, null=True)  # Field name made lowercase.
+    cancelby = models.CharField(db_column='CancelBy', max_length=20)  # Field name made lowercase.
+    canceldate = models.DateTimeField(db_column='CancelDate', blank=True, null=True)  # Field name made lowercase.
+    deleted = models.IntegerField(db_column='Deleted', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'project'
+
+
+class Task(models.Model):
+    id = models.IntegerField(primary_key=True)
+    projectid = models.IntegerField(db_column='ProjectId')  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=100)  # Field name made lowercase.
+    desc = models.CharField(db_column='Desc', max_length=2500)  # Field name made lowercase.
+    status = models.IntegerField(db_column='Status')  # Field name made lowercase.
+    startdate = models.DateTimeField(db_column='StartDate')  # Field name made lowercase.
+    enddate = models.DateTimeField(db_column='EndDate')  # Field name made lowercase.
+    departementid = models.IntegerField(db_column='DepartementId')  # Field name made lowercase.
+    userid = models.IntegerField(db_column='UserId')  # Field name made lowercase.
+    assigndate = models.DateTimeField(db_column='AssignDate')  # Field name made lowercase.
+    realstartdate = models.DateTimeField(db_column='RealStartDate')  # Field name made lowercase.
+    finishby = models.IntegerField(db_column='FinishBy')  # Field name made lowercase.
+    finishdate = models.DateTimeField(db_column='FinishDate')  # Field name made lowercase.
+    cancelby = models.IntegerField(db_column='CancelBy')  # Field name made lowercase.
+    canceldate = models.DateTimeField(db_column='CancelDate')  # Field name made lowercase.
+    closeby = models.IntegerField(db_column='CloseBy')  # Field name made lowercase.
+    closedate = models.DateTimeField(db_column='CloseDate')  # Field name made lowercase.
+    closereson = models.CharField(db_column='CloseReson', max_length=500)  # Field name made lowercase.
+    lastedit = models.DateTimeField(db_column='LastEdit')  # Field name made lowercase.
+    deleted = models.IntegerField(db_column='Deleted', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'task'
+
+
+class TaskStatus(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.IntegerField(db_column='Name')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'task_status'
+
+
+class ProjectStatus(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.IntegerField(db_column='Name')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'project_status'
+
+
+class Employee(models.Model):
+    id = models.IntegerField(db_column='Id', primary_key=True)  # Field name made lowercase.
+    empid = models.CharField(db_column='EmpId', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    depcode = models.CharField(db_column='DepCode', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    depname = models.CharField(db_column='DepName', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    ismanager = models.CharField(db_column='IsManager', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    ext = models.CharField(db_column='Ext', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    mobile = models.CharField(db_column='Mobile', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    email = models.CharField(db_column='Email', max_length=100, blank=True, null=True)  # Field name made lowercase.
+    jobtitle = models.CharField(db_column='JobTitle', max_length=200, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'employee'
+
+
+class Department(models.Model):
+    id = models.IntegerField(db_column='Id', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    managerid = models.CharField(db_column='ManagerId', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    depcode = models.IntegerField(db_column='DepCode', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'department'
--- tms/tms/settings.py
+++ tms/tms/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'tms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'tms',
+       'USER': 'root',
+       'PASSWORD': '',
+       'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+       'PORT': '3306',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
