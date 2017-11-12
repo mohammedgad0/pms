35e55a82a465d5da2548f9dadd797eab589f8f8d
sherif ibrahim kamel sakr <elsherif_sakr@hotmail.com>
2017-11-12 10:56:54 +0300

    update template

--- tms/tms/urls.py
+++ tms/tms/urls.py
@@ -1,21 +1,12 @@
-"""
-Definition of urls for tms. 
-"""
-
 from django.conf.urls import include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', tms.views.home, name='home'),
-    # url(r'^tms/', include('tms.tms.urls')),
+    url(r'^admin/', admin.site.urls),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # app/ -> Genetelella UI and resources
+    url(r'^project/', include('project.urls')),
+    url(r'^', include('project.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 ]
