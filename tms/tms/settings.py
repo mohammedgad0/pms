9262902239dffab097b0803ee9ff35b03694f44f
mgad <mohammedgad09@gmail.com>
2017-11-12 13:24:35 +0300

    update setting and title

--- tms/tms/settings.py
+++ tms/tms/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project'
+    'project.apps.ProjectConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
