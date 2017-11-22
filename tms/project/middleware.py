9ffe2bb6b938423abfe53ffc28f0ecf3a49f27b3
mgad <mohammedgad09@gmail.com>
2017-11-22 16:09:13 +0300

    session

--- tms/project/middleware.py
+++ tms/project/middleware.py
@@ -8,16 +8,16 @@ class ForceLangMiddleware:
         translation.activate(request.LANG)
         request.LANGUAGE_CODE = request.LANG
         if request.user.is_authenticated():
-            if not request.session['EmpID']:
-                email = request.user.email
-                emp = Employee.objects.filter(email= email)
-                # Get all data filtered by user email and set in session
-                for data in emp:
-                    request.session['EmpID'] = data.empid
-                    request.session['EmpName'] = data.empname
-                    request.session['DeptName'] = data.deptname
-                    request.session['Mobile'] = data.mobile
-                    request.session['DeptCode'] = data.deptcode
+            # if not request.session['EmpID']:
+            email = request.user.email
+            emp = Employee.objects.filter(email= email)
+            # Get all data filtered by user email and set in session
+            for data in emp:
+                request.session['EmpID'] = data.empid
+                request.session['EmpName'] = data.empname
+                request.session['DeptName'] = data.deptname
+                request.session['Mobile'] = data.mobile
+                request.session['DeptCode'] = data.deptcode
 class UserSeesionSet:
     def UserSessions(request):
         if request.user.is_authenticated():
