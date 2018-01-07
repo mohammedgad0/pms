703cced264fb9176d3d416bc0ce690cde735707e
mgad <mohammedgad09@gmail.com>
2018-01-07 12:43:25 +0300

    relation createdby

--- tms/project/views.py
+++ tms/project/views.py
@@ -1121,11 +1121,38 @@ def DashboardManager(request):
     dept_code = '2322'
     start_date = datetime.now() - relativedelta(years=1)
     end_date = datetime.now()
+    def dept_task_indicators(deptcode,startdate,enddate):
+        pass
+    import json
+    dept_data = get_object_or_404(Department,deptcode=dept_code)
+    dept_manager = dept_data.managerid
+    dept_internal_task_count = Task.objects.filter(
+    Q(createdby = dept_manager)&
+    Q(enddate__gte = start_date, startdate__lte = end_date)
+    ).count()
+
+    dept_external_task = Task.objects.filter(
+    Q(departement = dept_code)&
+    ~Q(createdby = dept_manager)&
+    Q(enddate__gte = start_date, startdate__lte = end_date)
+    )
+
+    TaskDict = {}
+    TaskDict.update({dept_data.deptname: dept_internal_task_count})
+    for data in dept_external_task:
+        tasks = Task.objects.filter(departement = data.departement, createdby = data.createdby)
+        task_count = tasks.count()
+        for name in tasks:
+            each_dept_count = tasks.filter(createdby = name.createdby).count()
+            TaskDict.update({name.createdby.deptname: each_dept_count})
+
+    js_data = json.dumps(TaskDict,ensure_ascii=False).encode('utf8')
 
     pie_tasks=_dept_tasks_statistics(dept_code,start_date,end_date)
     open_projects=_dept_open_pojects(dept_code,start_date,end_date)
     per_indicator = indicators(dept_code,start_date,end_date)
-    context = {"start_date":start_date,"end":end_date,
+    context = {"start_date":start_date,"end":end_date,"dept_task_count":dept_internal_task_count,
+                "dept_external_task":dept_external_task,"TaskDict":TaskDict,"js_data":js_data,
                'pie_tasks':pie_tasks,"per_indicator":per_indicator,'open_projects':open_projects
                }
     return render(request, 'project/dashboard_manager.html', context)
@@ -1170,7 +1197,6 @@ def _dept_open_pojects(deptcode,startdate,enddate):
         projectDict["InProgress"]=tasks.filter(status__exact="InProgress").count()
         projectList.append(projectDict)
     return projectList
-
 
 def indicators(deptcode,start_date,end_date):
     from django.db.models import F
